7bd17e8e58edd5775ecfcb8f40da6d49ff7a0836
Justin Azoff <justin.azoff@gmail.com>
2016-12-17 11:55:02 -0500

    bump bhr-client version requirement

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='bhr-bro',
-    version='0.1.1',
+    version='0.1.2',
     zip_safe=True,
     py_modules = ["bhr"],
     install_requires=[
-        "bhr_client>=0.14",
+        "bhr_client>=0.17",
         "dirq>=1.6.1",
     ],
     entry_points = {
